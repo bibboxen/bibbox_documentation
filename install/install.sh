5a501fdaa0cd0fd4e24d70dcb3502d40bc7529f6
Jesper Kristensen <cableman@linuxdev.dk>
2016-12-06 10:42:32 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -16,7 +16,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ## Define the release file.
 URL="https://github.com/bibboxen/bibbox/releases/download/v1.1.0/"
 FILE="v1.1.0.tar.gz"
-VERSION="v1.0.0"
+VERSION="v1.1.0"
 
 ## Define colors.
 BOLD=$(tput bold)
