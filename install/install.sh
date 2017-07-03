a6a5c70a1b2804c1ff8a66a03b3b0c63dfe08b28
Jesper Kristensen <cableman@linuxdev.dk>
2017-07-03 14:43:54 +0200

    Issue #BIB-276: Updated version number in install script

--- install/install.sh
+++ install/install.sh
@@ -14,7 +14,7 @@ cd ~/
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ## Define the release file.
-VERSION="v1.4.2"
+VERSION="v1.4.3"
 URL="https://github.com/bibboxen/bibbox/releases/download/${VERSION}/"
 FILE="${VERSION}.tar.gz"
 
